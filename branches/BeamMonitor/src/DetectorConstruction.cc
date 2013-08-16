8caa6400d25b468c29e34b0cd624c6f51ed07015
changhyeuk <changhyeuk@2a2a7d2a-e976-4448-7288-cf2a7092d2e8>
2013-08-16 08:17:55 +0000

    The Monitor Geometry tested

--- branches/BeamMonitor/src/DetectorConstruction.cc
+++ branches/BeamMonitor/src/DetectorConstruction.cc
@@ -53,24 +53,67 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   BeamLineComponentManager bcm(430.0 * MeV, 0.5);
   bcm.SetBGMaterial("G4_Galactic");
 
-  DriftSpace    D(0.4 * m);
-  Block         BKAPTON(0.01 * m, 0.1 * m,"G4_KAPTON");
-  Block         BAIR(0.01 * m, 0.1 * m,"G4_AIR");
+  DriftSpace    D(0.1 * m);
+  Block         BPET(25.0 * um, 25.0 * um, "G4_POLYETHYLENE");
+  Block         BCU(50.0 * um, 50.0 * um,"G4_Cu");
+  Block         BPG(0.2 * mm, 0.2 * mm, "G4_Pyrex_Glass");
+  Block         BKAPTON(200.0 * um, 200.0 * um,"G4_KAPTON");
+  Block         BGAIR(0.005 * m, 0.005 * m,"G4_AIR");
+  Block         BAIR1(0.1 * m, 0.1 * m,"G4_AIR");
+  Block         BAIR2(0.5 * m, 0.5 * m,"G4_AIR");
   WaterPhantom  WP(0.2 * m, 0.2 * m, 0.1 * m, "G4_WATER");
- 
-  bcm.Add(D.New(0.4 * m));
-  //bcm.Add(SMx.New(0.5 * tesla));
-  //bcm.Add(D.New(0.3 * m));
-  //bcm.Add(BKAPTON.New(0.0002 * m));
-  //bcm.Add(BAIR.New(0.1 * m));
-  //bcm.Add(RF);
-  //bcm.Add(BAIR.New(0.1 * m));
-  //bcm.Add(MLC);
-  //bcm.Add(BAIR.New(0.1 * m));
-  //bcm.Add(WP.New());
-
-
   VirtualMonitor mon(8.0 * cm, 8.0 * cm);
+    
+  // Beam Line
+  bcm.Add(D.New(0.1 * m));
+  // Kepton Window
+  bcm.Add(BKAPTON.New(200.0 * um));
+  // Air Drift
+  bcm.Add(BAIR1.New(0.1*m));
+  
+  // Monitor
+    // Monitor Cover Front
+    bcm.Add(BPET.New(25.0 * um));
+    bcm.Add(BGAIR.New(0.005 * m));
+    
+    // Dose Monitor 1
+    bcm.Add(BPET.New(25.0 * um));
+    bcm.Add(BGAIR.New(0.005 * m));
+    bcm.Add(BCU.New(50.0 * um));
+    bcm.Add(BPG.New(0.2 * mm));
+        // Gap
+        bcm.Add(BGAIR.New(0.005 * m));
+    // Dose Monitor 2
+    bcm.Add(BPET.New(25.0 * um));
+    bcm.Add(BGAIR.New(0.005 * m));
+    bcm.Add(BCU.New(50.0 * um));
+    bcm.Add(BPG.New(0.2 * mm));
+        // Gap
+        bcm.Add(BGAIR.New(0.005 * m));
+    // Position Monitor 1
+    bcm.Add(BPET.New(25.0 * um));
+    bcm.Add(BGAIR.New(0.005 * m));
+    bcm.Add(BCU.New(50.0 * um));
+    bcm.Add(BPG.New(0.2 * mm));
+        // Gap
+        bcm.Add(BGAIR.New(0.005 * m));
+    // Position Monitor 2
+    bcm.Add(BPET.New(25.0 * um));
+    bcm.Add(BGAIR.New(0.005 * m));
+    bcm.Add(BCU.New(50.0 * um));
+    bcm.Add(BPG.New(0.2 * mm));
+    
+    //Monitor Cover Back
+    bcm.Add(BGAIR.New(0.005 * m));
+    bcm.Add(BPET.New(25.0 * um));
+    
+  // Air Drift
+  bcm.Add(mon.New());
+  bcm.Add(BAIR2.New(0.5*m));
+  bcm.Add(mon.New());
+    
+  // Water Phantom
+  //bcm.Add(WP.New());
 
 
   G4VPhysicalVolume* pv = bcm.GenerateVolume();
