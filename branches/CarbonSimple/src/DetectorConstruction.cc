84f452c65ac3e8a37f46ad5097363a28cff4e729
changhyeuk <changhyeuk@2a2a7d2a-e976-4448-7288-cf2a7092d2e8>
2013-09-27 02:33:19 +0000

    For construction of the beam line, the ridge  and MLC was inserted

--- branches/CarbonSimple/src/DetectorConstruction.cc
+++ branches/CarbonSimple/src/DetectorConstruction.cc
@@ -76,10 +76,50 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
    */
   Block         BKAPTON(0.01 * m, 0.1 * m,"G4_KAPTON");
   Block         BAIR(0.01 * m, 0.1 * m,"G4_AIR");
+  RidgeFilter* RF = new RidgeFilter(30.*cm, 30.*cm, "G4_Al", "G4_AIR");
+    RF->Add2DCrxPoint(1.*mm,   2.*mm);
+    RF->Add2DCrxPoint(2.*mm,   4.*mm);
+    RF->Add2DCrxPoint(3.*mm,   8.*mm);
+    RF->Add2DCrxPoint(4.*mm,   16.*mm);
+    RF->Add2DCrxPoint(5.*mm,   32.*mm);
+    RF->Add2DCrxPoint(6.*mm,   42.*mm);
+    RF->Add2DCrxPoint(7.*mm,   42.*mm);
+    RF->AddSpace(3.0 * mm);
+    
+  MultiLeafCollimator* MLC
+    = new MultiLeafCollimator(40.*cm, 40.*cm, 10.*cm, "G4_Al", "G4_AIR");
+    MLC->DefineLeaf(5, 1.0*cm);
+    MLC->DefineLeaf(5, 2.0*cm); // leafs are totally 40(left) + 40(right)
+    
+    MLC->SetLeaf(-10,  0.*cm, 0.*cm);
+    MLC->SetLeaf(- 9, -1.*cm, 1.*cm);
+    MLC->SetLeaf(- 8, -2.*cm, 2.*cm);
+    MLC->SetLeaf(- 7, -3.*cm, 3.*cm);
+    MLC->SetLeaf(- 6, -4.*cm, 4.*cm);
+    
+    MLC->SetLeaf(- 5, -10.*cm, -10.*cm);
+    MLC->SetLeaf(- 4, -11.*cm, -9.*cm);
+    MLC->SetLeaf(- 3, -12.*cm, -8.*cm);
+    MLC->SetLeaf(- 2, -13.*cm, -7.*cm);
+    MLC->SetLeaf(- 1, -14.*cm, -6.*cm);
+    
+    MLC->SetLeaf( 1, 5.*cm, 5.*cm);
+    MLC->SetLeaf( 2, 4.*cm, 6.*cm);
+    MLC->SetLeaf( 3, 3.*cm, 7.*cm);
+    MLC->SetLeaf( 4, 2.*cm, 8.*cm);
+    MLC->SetLeaf( 5, 1.*cm, 9.*cm);
+    
+    MLC->SetLeaf( 6, 1.*cm, 9.*cm);
+    MLC->SetLeaf( 7, 2.*cm, 8.*cm);
+    MLC->SetLeaf( 8, 3.*cm, 7.*cm);
+    MLC->SetLeaf( 9, 4.*cm, 6.*cm);
+    MLC->SetLeaf(10, 5.*cm, 5.*cm);
   WaterPhantom  WP(0.3 * m, 0.3 * m, 0.4 * m, "G4_WATER");
-  //CalorimeterBlock WB(0.3 * m, 0.3 * m, 0.4 * m, "G4_WATER");
- 
+    
   bcm.Add(D.New(0.4 * m));
+  bcm.Add(RF);
+  bcm.Add(BAIR.New(0.1 * m));
+  bcm.Add(MLC);
   bcm.Add(WP.New());
     //bcm.Add(WB.New());
     
